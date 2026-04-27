12e87b16be95cc6a21087864828a414d902b077c
brycenHanson <brycen.hanson@ndsu.edu>
2026-04-27 16:53:31 -0500

    Fix import formatting for yfinance in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import yfinance as yf
+import yfinance as yf 
 import matplotlib.pyplot as plt
 from ta.momentum import RSIIndicator
 
